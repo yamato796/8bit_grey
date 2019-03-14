7cddace0b3e7496826255c7cba0d11e3f85fbcc2
yamato796 <30308311+yamato796@users.noreply.github.com>
2019-03-14 14:57:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import time
 from io   import BytesIO
 
+#import datetime
 import os 
 import cv2
 import math
@@ -9,22 +10,32 @@ import base64
 import logging
 import random
 
-
+#img = np.ones((65536,65536,1),np.uint8)
+length = 32
+width = 32
+size_x = length*length*2
+size_y = width*width*2
+img = np.ones((size_x,size_y,1),np.uint8)
+#img*=255
 x =0
 y =0
-img = np.zeros((131072,131072,1),np.uint8)
-for i in range(0,255):
-	for j in range(0,255):
-		for k in range(0,255):
-			for l in range(0,255):
-				img[x  ][y  ] = i
-				img[x+1][y  ] = j
-				img[x  ][y+1] = k
-				img[x+1][y+1] = l
-				if x <131070:
+#img = np.zeros((131072,131072,1),np.uint8)
+for i in range(0,length):
+	for j in range(0,length):
+		for k in range(0,length):
+			for l in range(0,length):
+				print x,y
+				img[x  ][y  ] = i+random.randint(0,200)
+				img[x+1][y  ] = j+random.randint(0,200)
+				img[x  ][y+1] = k+random.randint(0,200)
+				img[x+1][y+1] = l+random.randint(0,200)
+				if x < size_x-2:
 					x=x+2
-				else:
+				elif y< size_y-2:
 					x=0
 					y=y+2
 
 cv2.imwrite("./img.png",img)
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
